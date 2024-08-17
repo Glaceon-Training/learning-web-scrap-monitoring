864a03e8dfb93329ed6926f86987589ab4147f4a
glaceon033 <glaceon033@gmail.com>
2024-08-17 17:28:41 +0700

    First package: web scrap earthquake live information & main app

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Earthquake detection app:
+Modularization with Function,
+Modularization with Package
+"""
+from learn_recent_earthquake2 import data_extraction, show_data
+
+if __name__ == '__main__':
+    print('Main App\n')
+    result = data_extraction()
+    show_data(result)
