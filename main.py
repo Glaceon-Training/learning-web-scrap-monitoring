7bf6aa4de034dc3525d2e2e4d1495a8203a5d48d
glaceon033 <glaceon033@gmail.com>
2024-08-28 22:01:34 +0700

    procedural recent earthquake bmkg v000

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     print('Package description is', recent_earthquake2.description)
     result = recent_earthquake2.data_extraction()
     recent_earthquake2.show_data(result)
+
